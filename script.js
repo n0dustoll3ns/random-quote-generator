42927a65b3a3f34a6dd596965a29620bc9d11f91
n0dustoll3ns <fominykh.dg@gmail.com>
2022-04-25 17:12:06 +0700

    complete with simple design and statick mock data wothout API

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function generate() {
   };
 
   let authors = Object.keys(quotes);
+  let author = authors[Math.floor(Math.random() * authors.length)]
 
+  let quote = quotes[author];
+  document.getElementById("quote").innerHTML = quote;
+  document.getElementById("author").innerHTML = author;
   console.log(authors)
 }
