85af93177d7141d625e211d4058975f1b0937d60
n0dustoll3ns <fominykh.dg@gmail.com>
2022-04-25 16:52:25 +0700

    setup script to pop random quote

---
+++ script.js
@@ -0,0 +1,11 @@
+function generate() {
+  let quotes = {
+    "Albert Einstein": '"Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole of nature and it\'s beauty." ',
+    "Jamie Ford": '"The library is like a candy store where everything is free."',
+    "Charlotte Eriksson": '"I woke up early and took the first train to take me away from the city. The noise and all its people. I was alone on the train and had no idea where I was going, and that’s why I went there. Two hours later we arrived in a small town, one of those towns with one single coffee shop and where everyone knows each other’s name. I walked for a while until I found the water, the most peaceful place I know. There I sat and stayed the whole day, with nothing and everything on my mind, cleaning my head. Silence, I learned, is some times the most beautiful sound."'
+  };
+
+  let authors = Object.keys(quotes);
+
+  console.log(authors)
+}
