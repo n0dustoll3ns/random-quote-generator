edcbe47605cce298165592d905e001c76fddead1
n0dustoll3ns <fominykh.dg@gmail.com>
2022-04-26 17:17:57 +0700

    added API integration

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function generate() {
-  let quotes = {
-    "Albert Einstein": '"Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole of nature and it\'s beauty." ',
-    "Jamie Ford": '"The library is like a candy store where everything is free."',
-    "Charlotte Eriksson": '"I woke up early and took the first train to take me away from the city. The noise and all its people. I was alone on the train and had no idea where I was going, and that’s why I went there. Two hours later we arrived in a small town, one of those towns with one single coffee shop and where everyone knows each other’s name. I walked for a while until I found the water, the most peaceful place I know. There I sat and stayed the whole day, with nothing and everything on my mind, cleaning my head. Silence, I learned, is some times the most beautiful sound."'
+  const options = {
+    method: 'GET',
   };
-
-  let authors = Object.keys(quotes);
-  let author = authors[Math.floor(Math.random() * authors.length)]
-
-  let quote = quotes[author];
-  document.getElementById("quote").innerHTML = quote;
-  document.getElementById("author").innerHTML = author;
-  console.log(authors)
+  fetch('https://api.quotable.io/random', options)
+    .then(response => response.json())
+    .then(response => {
+      document.getElementById("quote").innerHTML = response.content;
+      document.getElementById("author").innerHTML = response.author;
+    })
+    .catch(err => {
+      document.getElementById("quote").innerHTML = "Carrier pigeon with a message lost on the way";
+      document.getElementById("author").innerHTML = "";
+    });
 }
+
